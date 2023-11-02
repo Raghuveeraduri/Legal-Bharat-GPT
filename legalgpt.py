48c8b69cf7d98b63869c6da18dfdba1fcd9c476f
Raghuveeraduri <76482531+Raghuveeraduri@users.noreply.github.com>
2023-11-03 00:20:01 +0530

    legalgpt.py

--- legalgpt.py
+++ legalgpt.py
@@ -12,9 +12,7 @@ with file_handler:
     if st.button("🔃 Refresh"):
         st.cache_data.clear()
 hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 # Define conversation examples
