d4f3e3c5f9d3b6eb006b822e79bf2bf8c95ca936
Raghuveeraduri <76482531+Raghuveeraduri@users.noreply.github.com>
2023-11-03 00:09:51 +0530

    legalgpt.py

--- legalgpt.py
+++ legalgpt.py
@@ -6,8 +6,6 @@ st.set_page_config(page_title="Bharat Legal GPT",page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# This is a header. This is an *extremely* cool app!"
     })
 file_handler = st.container()
